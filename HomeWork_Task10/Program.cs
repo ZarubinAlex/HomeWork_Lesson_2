7672c2f167535b7efbdc935f87dd8a5183496088
ALex <sknnov@yandex.ru>
2022-11-25 23:17:35 +0300

    HomeWork Task 10 & 13

---
+++ HomeWork_Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+ int ShowNumber(string txt, int number)
+{
+    int digit = Convert.ToInt32(txt[number - 1].ToString());
+    return digit;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+string digit = Console.ReadLine();
+
+int numberOfShowDigit = 2;
+
+Console.WriteLine(numberOfShowDigit + "-я цифра числа " + digit + ": " + ShowNumber(digit, numberOfShowDigit));
+Console.WriteLine();
---
+++ HomeWork_Task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+ int ShowNumber(string txt, int number)
+{
+    int digit = Convert.ToInt32(txt[number - 1].ToString());
+    return digit;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+string digit = Console.ReadLine();
+
+int numberOfShowDigit = 3;
+
+if(numberOfShowDigit > digit.Length) 
+    Console.WriteLine(numberOfShowDigit + "-й цифры нет");
+else
+    Console.WriteLine(numberOfShowDigit + "-я цифра числа " + digit + ": " + ShowNumber(digit, numberOfShowDigit));
+
+Console.WriteLine();
